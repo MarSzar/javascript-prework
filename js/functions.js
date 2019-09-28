a5d75c6f59f946c11f7f8d353e2325ddb25c9b19
Marta Szargało <marta.szargalo@tlen.pl>
2019-09-28 23:49:16 +0200

    Add new block if..else if

--- js/functions.js
+++ js/functions.js
@@ -26,6 +26,7 @@ if(randomNumber == 1){
   printMessage('Mój ruch to: ' + computerMove);
 }
 
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -41,4 +42,27 @@ if(playerInput == '1'){
 } else if (playerInput == '3') {
   playerMove = 'nożyce';
   printMessage('Twój ruch to: ' + playerMove);
+}
+
+
+if (computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ty przegrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Ty przegrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Ty przegrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+} else if (playerMove == 'nieznany ruch'){
+  printMessage('Wykonaj swój ruch');
 }
